99c035d367967424ef6b26b7e6506f0c1550059d
Hari Narayan <harinarayanmr@aol.com>
2025-02-14 12:09:25 +0530

    setting up styles and home

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -1,8 +1,15 @@
 <script>
     import { Link } from "svelte-routing";
+    import Nav from "./Nav.svelte";
 </script>
 
 <main>
+    <Nav />
+    <h1>
+        <span>Introducing</span>
+        Savr
+    </h1>
+    hey there
     <Link to="/scan">
         <button>Scan page</button>
     </Link>
---
+++ src/components/Nav.svelte
@@ -0,0 +1,48 @@
+<script>
+    import { Link } from "svelte-routing";
+</script>
+
+<main>
+    <div class="nav">
+        <div class="nav__icon"></div>
+        <div class="nav__stats">
+            <h5>Hari</h5>
+        </div>
+    </div>
+</main>
+
+<style>
+    .nav {
+        display: flex;
+        background-color: white;
+        z-index: 99;
+        margin-bottom: 0.5rem;
+        position: relative;
+    }
+    .nav__placeholder {
+        flex: 1;
+        color: transparent;
+    }
+    .nav__icon img {
+        scale: 1.2;
+    }
+    .nav__stats {
+        display: flex;
+        align-items: flex-start;
+        gap: 0.5rem;
+    }
+    .nav__dropdown {
+        position: absolute;
+        left: 0;
+        top: 0;
+        background-color: white;
+        width: 100%;
+        height: 100vh;
+        display: none;
+    }
+    .nav__dropdown p {
+        font-size: 1.2rem;
+        margin-bottom: 1rem;
+        font-weight: 300;
+    }
+</style>
