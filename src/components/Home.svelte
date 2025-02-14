8b21b74c89c1cc3ea7130ed6bf7b7c95e7673eb8
Hari Narayan <harinarayanmr@aol.com>
2025-02-14 12:27:20 +0530

    testing out home

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -3,16 +3,23 @@
     import Nav from "./Nav.svelte";
 </script>
 
+<Nav />
 <main>
-    <Nav />
-    <h1>
-        <span>Introducing</span>
-        Savr
-    </h1>
-    hey there
-    <Link to="/scan">
-        <button>Scan page</button>
-    </Link>
+    <div class="contents">
+        <h1>
+            <span>Introducing</span>
+            Savr
+        </h1>
+        <p>hey there</p>
+    </div>
+    <div class="buttons">
+        <Link to="/login">
+            <button>Login</button>
+        </Link>
+        <Link to="/scan">
+            <button class="outline">Scan page</button>
+        </Link>
+    </div>
 </main>
 
 <style>
