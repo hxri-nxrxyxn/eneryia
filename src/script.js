5d3d1471b9aaa712df838be1aa6e3211a33937de
Sebastian <sebastianabraham2006@gmail.com>
2025-02-14 15:14:01 +0530

    created function middleware

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
 import { App } from '@capacitor/app';
 import { navigate } from "svelte-routing";
 
+const baseUrl = "https://api.laddu.cc:8080";
+
 function handleBackButton(fallbackUrl) {
     sessionStorage.setItem("fallbackPage", fallbackUrl);
 
@@ -13,6 +15,27 @@ function handleBackButton(fallbackUrl) {
             App.exitApp();
         }
     });
+}
+
+async function signup(data) {
+    try {
+        const response = await fetch(`${baseUrl}/register`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+        const res = await response.json();
+        if (!response.ok) {
+            alert(res.message);
+            return;
+        }
+        navigate("/dashboard", { replace: true });
+
+    } catch (error) {
+        alert(`Error creating data:${error}`);
+    }
 }
 
 export { handleBackButton };  
