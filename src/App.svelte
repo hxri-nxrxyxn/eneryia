3ca701bbcfb6e70ca68cf37a5ddf449a1c657d06
Hari Narayan <harinarayanmr@aol.com>
2025-02-14 11:13:15 +0530

    fixed issues

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,14 @@
 <script>
-  import {Router, Route} from "svelte-routing"
-  import Home from "./components/Home.svelte"
-  import Scan from "./components/Scan.svelte"
+    import { Router, Route } from "svelte-routing";
+    import Home from "./components/Home.svelte";
+    import Scan from "./components/Scan.svelte";
 </script>
 
 <main>
-    <Route path="/" component={Home}/>
-        <Route path="/scan" component={Scan}>
     <Router>
-        </Router>
-    </main>
+        <Route path="/" component={Home} />
+        <Route path="/scan" component={Scan} />
+    </Router>
+</main>
 
 <style></style>
