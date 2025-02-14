fa3d0ad20cc45abadfdb7060240bb948f9cc7e7b
Sebastian <sebastianabraham2006@gmail.com>
2025-02-14 19:26:15 +0530

    implmented create ingredient functiionality

--- api/controller/FoodFunctions.go
+++ api/controller/FoodFunctions.go
@@ -2,11 +2,35 @@ package controller
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/savr/models"
 	"gorm.io/gorm"
 )
 
-func FOod(db *gorm.DB) func(*fiber.Ctx) error {
+func CreateIngredient(db *gorm.DB) func(*fiber.Ctx) error {
 	return func(c *fiber.Ctx) error {
-		return c.SendString("Get User")
+		ingredient := new(models.Ingredient)
+
+		err := c.BodyParser(ingredient)
+		if err != nil {
+			return c.Status(400).JSON(fiber.Map{
+				"message": "Could not parse Body",
+				"error":   err.Error(),
+			})
+		}
+
+		err = db.Create(ingredient).Error
+		if err != nil {
+
+
+			return c.Status(501).JSON(fiber.Map{
+				"message": "Could not create ingredient",
+				"error":   err.Error(),
+			})
+		}
+
+		return c.Status(201).JSON(fiber.Map{
+			"message": "User created",
+			"data":    ingredient,
+		})
 	}
 }
