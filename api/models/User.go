516c7d3061467e5fdcda94471c09a26d728caebc
Sebastian <sebastianabraham2006@gmail.com>
2025-02-14 15:47:54 +0530

    updated user model

--- api/models/User.go
+++ api/models/User.go
@@ -5,10 +5,13 @@ import (
 )
 
 type User struct {
-	UserID   uint    `gorm:"primaryKey;autoIncrement" json:"id"`
-	Name     *string `gorm:"type:varchar(255)" json:"name"`
-	Email    string  `gorm:"type:varchar(255);unique;not null" json:"email"`
-	Password string  `gorm:"type:varchar(255);not null" json:"password"`
+	UserID     uint    `gorm:"primaryKey;autoIncrement" json:"id"`
+	Name       *string `gorm:"type:varchar(255)" json:"name"`
+	Email      string  `gorm:"type:varchar(255);unique;not null" json:"email"`
+	Password   string  `gorm:"type:varchar(255);not null" json:"password"`
+	Preference *string `gorm:"type:varchar(255);" json:"preference"`
+	Intake     int     `gorm:"type:int" json:"intake"`
+	MPD        int     `gorm:"type:int" json:"mpd"`
 }
 
 func MigrateUser(db *gorm.DB) error {
