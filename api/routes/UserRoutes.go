2a42f1dc48043230ca6c2da43cd7c5f2191bed2e
Sebastian <sebastianabraham2006@gmail.com>
2025-02-14 12:21:10 +0530

    created basic user routes

---
+++ api/routes/UserRoutes.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
+
+func UserRoutes(db *gorm.DB, app *fiber.App) {
+	api := app.Group("/api/v1")
+	api.Get("/users", controller.GetUsers(db))
+	api.Get("/users/:id", controller.GetUser(db))
+	api.Patch("/users/:id", controller.UpdateUser(db))
+	api.Post("/register", controller.CreateUser(db))
+	api.Post("/login", controller.Login(db))
+}
