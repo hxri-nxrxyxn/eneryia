064511fdb162da43c7dfd4d535b3d3c3822521b1
Sebastian <sebastianabraham2006@gmail.com>
2025-02-14 19:30:51 +0530

    added get indredient functionality

--- api/controller/FoodFunctions.go
+++ api/controller/FoodFunctions.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"strings"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/savr/models"
 	"gorm.io/gorm"
@@ -21,7 +23,6 @@ func CreateIngredient(db *gorm.DB) func(*fiber.Ctx) error {
 		err = db.Create(ingredient).Error
 		if err != nil {
 
-
 			return c.Status(501).JSON(fiber.Map{
 				"message": "Could not create ingredient",
 				"error":   err.Error(),
@@ -31,6 +32,32 @@ func CreateIngredient(db *gorm.DB) func(*fiber.Ctx) error {
 		return c.Status(201).JSON(fiber.Map{
 			"message": "User created",
 			"data":    ingredient,
+		})
+	}
+}
+
+func GetIngredient(db *gorm.DB) func(*fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		id := c.Params("id")
+
+		user := new(models.User)
+		err := db.Where("ingredient_id = ?", id).First(user).Error
+		if err != nil {
+			if strings.Contains(err.Error(), "record not found") {
+				return c.Status(404).JSON(fiber.Map{
+					"message": "Ingredient not found",
+				})
+			}
+
+			return c.Status(500).JSON(fiber.Map{
+				"message": "Could not retrieve Ingredient",
+				"error":   err.Error(),
+			})
+		}
+
+		return c.Status(200).JSON(fiber.Map{
+			"message": "Retrieved user",
+			"data":    user,
 		})
 	}
 }
