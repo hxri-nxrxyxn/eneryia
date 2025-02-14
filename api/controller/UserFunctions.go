634da007f4cb7febd0a546397098cc7083dbf61b
Sebastian <sebastianabraham2006@gmail.com>
2025-02-15 00:54:00 +0530

    added update user details functions

--- api/controller/UserFunctions.go
+++ api/controller/UserFunctions.go
@@ -109,7 +109,43 @@ func GetUser(db *gorm.DB) func(*fiber.Ctx) error {
 
 func UpdateUser(db *gorm.DB) func(*fiber.Ctx) error {
 	return func(c *fiber.Ctx) error {
-		return c.SendString("Get User")
+		id := c.Params("id")
+		user := new(models.User)
+
+		err := db.Where("user_id = ?", id).First(user).Error
+		if err != nil {
+			if strings.Contains(err.Error(), "record not found") {
+				return c.Status(404).JSON(fiber.Map{
+					"message": "User not found",
+				})
+			}
+
+			return c.Status(500).JSON(fiber.Map{
+				"message": "Could not retrieve user",
+				"error":   err.Error(),
+			})
+		}
+
+		err = c.BodyParser(user)
+		if err != nil {
+			return c.Status(400).JSON(fiber.Map{
+				"message": "Could not parse Body",
+				"error":   err.Error(),
+			})
+		}
+
+		err = db.Save(user).Error
+		if err != nil {
+			return c.Status(501).JSON(fiber.Map{
+				"message": "Could not update user",
+				"error":   err.Error(),
+			})
+		}
+
+		return c.Status(200).JSON(fiber.Map{
+			"message": "User updated",
+			"data":    user,
+		})
 	}
 }
 
