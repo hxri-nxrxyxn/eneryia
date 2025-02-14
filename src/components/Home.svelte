0dea5ec54d07cf016704a7209be9a40d4f9ca913
Hari Narayan <harinarayanmr@aol.com>
2025-02-14 12:35:18 +0530

    added universal scripts for extra funcs

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -10,7 +10,18 @@
             <span>Introducing</span>
             Savr
         </h1>
-        <p>hey there</p>
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta
+            doloremque, dolor impedit fugiat sint et error a ipsam tenetur.
+            Impedit minus, amet laudantium deserunt tenetur animi, quos sit ipsa
+            dolores magni nisi tempora explicabo. Totam sunt natus ea, nihil
+            animi similique accusantium maiores aliquam recusandae officiis,
+            voluptas adipisci quod magni quasi sequi unde ex vero at ratione
+            doloremque quam nesciunt harum facere distinctio. Repellat qui rerum
+            dolores fugit sunt eligendi animi incidunt, architecto quaerat! At
+            suscipit incidunt repudiandae in dicta, laboriosam facilis
+            praesentium nisi accusamus illum ea nulla sed saepe.
+        </p>
     </div>
     <div class="buttons">
         <Link to="/login">
