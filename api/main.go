e23689ff4445962fcd132476fc9e9303120e70c0
Sebastian <sebastianabraham2006@gmail.com>
2025-02-14 12:24:17 +0530

    implemented database connectivity

--- api/main.go
+++ api/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/savr/database"
 )
 
 func main() {
@@ -13,6 +16,11 @@ func main() {
 		AllowMethods: "*",
 		AllowHeaders: "Content-Type,Authorization",
 	}))
+
+	db, err := database.NewConnection()
+	if err != nil {
+		log.Fatal("Could not load database")
+	}
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
