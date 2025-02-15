4fa68d8901819283a9935313e17ae2e8b6651157
Hari Narayan <harinarayanmr@aol.com>
2025-02-15 10:38:07 +0530

    added links to scan2O

--- src/components/Scan.svelte
+++ src/components/Scan.svelte
@@ -1,66 +1,70 @@
 <script>
-  import { Camera, CameraResultType, CameraSource } from "@capacitor/camera";
-  import NavBot from "./NavBot.svelte";
-  import {
-    checkPermission,
-    startScanning,
-    stopScanning,
-    logout,
-    checkUser,
-    runAI,
-  } from "../script";
-  import { App } from "@capacitor/app";
-  window.scrollTo({ top: 0, behavior: "smooth" });
+    import { Camera, CameraResultType, CameraSource } from "@capacitor/camera";
+    import NavBot from "./NavBot.svelte";
+    import { Link } from "svelte-routing";
+    import {
+        checkPermission,
+        startScanning,
+        stopScanning,
+        logout,
+        checkUser,
+        runAI,
+    } from "../script";
+    import { App } from "@capacitor/app";
+    window.scrollTo({ top: 0, behavior: "smooth" });
 
-  let photoUrl = "";
+    let photoUrl = "";
 
-  App.addListener("backButton", () => {
-    stopScanning();
-  });
+    App.addListener("backButton", () => {
+        stopScanning();
+    });
 
-  checkUser();
+    checkUser();
 
-  async function takePhoto() {
-    try {
-      const image = await Camera.getPhoto({
-        quality: 40,
-        source: CameraSource.Camera, // Use "CameraSource.Photos" for gallery
-        resultType: CameraResultType.Base64, // "Uri" returns the image URL
-      });
+    async function takePhoto() {
+        try {
+            const image = await Camera.getPhoto({
+                quality: 40,
+                source: CameraSource.Camera, // Use "CameraSource.Photos" for gallery
+                resultType: CameraResultType.Base64, // "Uri" returns the image URL
+            });
 
-      console.log('t1')
-      await runAI(image.base64String);
-    } catch (error) {
-      alert(error);
+            console.log("t1");
+            await runAI(image.base64String);
+        } catch (error) {
+            alert(error);
+        }
     }
-  }
 </script>
 
 <NavBot selection="Plus" />
 <main>
-  <div class="contents">
-    <h1>
-      <span>Add to</span>
-      <br />
-      Inventory
-    </h1>
-    <div class="form">
-      <h3>with Manual Input</h3>
-      <br />
-      <input type="text" placeholder="Enter item" />
-      <button>ADD</button>
+    <div class="contents">
+        <h1>
+            <span>Add to</span>
+            <br />
+            Inventory
+        </h1>
+        <div class="form">
+            <h3>with Manual Input</h3>
+            <br />
+            <input type="text" placeholder="Enter item" />
+            <button>ADD</button>
+        </div>
+        <h3>with Photo</h3>
+        <button onclick={takePhoto}>click me</button>
+        <h3>with Barcode</h3>
+        <button onclick={startScanning}>Bar Code</button>
+        <h3>Get me outta here</h3>
+        <button onclick={logout}>Logout</button>
+        <Link to="/scan2">
+            <button>scan2</button>
+        </Link>
     </div>
-    <h3>with Photo</h3>
-    <button onclick={takePhoto}>click me</button>
-    <h3>with Barcode</h3>
-    <button onclick={startScanning}>Bar Code</button>
-    <h3>Get me outta here</h3>
-    <button onclick={logout}>Logout</button>
-  </div>
 </main>
 
 <style>
-  h3 {
-    margin-top: 1rem;
-  }
+    h3 {
+        margin-top: 1rem;
+    }
 </style>
