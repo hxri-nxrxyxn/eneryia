821f73ab85dc8194f38dd959b5172add1dfc15cb
Sebastian <sebastianabraham2006@gmail.com>
2025-02-15 09:20:38 +0530

    help me :(

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,35 @@
 <script>
-    import { Router, Route } from "svelte-routing";
-    import Home from "./components/Home.svelte";
-    import Scan from "./components/Scan.svelte";
-    import Signup from "./components/Signup.svelte";
-    import Collect from "./components/Collect.svelte";
-    import Terms from "./components/Terms.svelte";
-    import Dashboard from "./components/Dashboard.svelte";
-    import Recipe from "./components/Recipe.svelte";
-    import Inventory from "./components/Inventory.svelte";
-    import Scan2 from "./components/Scan2.svelte";
-    import Keyboard from "./components/Keyboard.svelte";
-    import Eye from "./components/Eye.svelte";
-    import Barcode from "./components/Barcode.svelte";
+  import { Router, Route } from "svelte-routing";
+  import Home from "./components/Home.svelte";
+  import Scan from "./components/Scan.svelte";
+  import Signup from "./components/Signup.svelte";
+  import Collect from "./components/Collect.svelte";
+  import Terms from "./components/Terms.svelte";
+  import Dashboard from "./components/Dashboard.svelte";
+  import Recipe from "./components/Recipe.svelte";
+  import Inventory from "./components/Inventory.svelte";
+  import Scan2 from "./components/Scan2.svelte";
+  import Keyboard from "./components/Keyboard.svelte";
+  import Eye from "./components/Eye.svelte";
+  import Barcode from "./components/Barcode.svelte";
 </script>
 
 <main>
-    <Router>
-        <Route path="/" component={Dashboard} />
-        <Route path="/login" component={Home} />
-        <Route path="/scan" component={Scan} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/collect" component={Collect} />
-        <Route path="/terms" component={Terms} />
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/recipe" component={Recipe} />
-        <Route path="/inventory" component={Inventory} />
-        <Route path="/scan2" component={Scan2} />
-        <Route path="/keyboard" component={Keyboard} />
-        <Route path="/eye" component={Eye} />
-        <Route path="/barcode" component={Barcode} />
-    </Router>
+  <Router>
+    <Route path="/" component={Dashboard} />
+    <Route path="/login" component={Home} />
+    <Route path="/scan" component={Scan} />
+    <Route path="/signup" component={Signup} />
+    <Route path="/collect" component={Collect} />
+    <Route path="/terms" component={Terms} />
+    <Route path="/dashboard" component={Dashboard} />
+    <Route path="/recipe" component={Recipe} />
+    <Route path="/inventory" component={Inventory} />
+    <Route path="/scan2" component={Scan2} />
+    <Route path="/keyboard" component={Keyboard} />
+    <Route path="/eye" component={Eye} />
+    <Route path="/barcode" component={Barcode} />
+  </Router>
 </main>
 
 <style></style>
--- src/components/Recipe.svelte
+++ src/components/Recipe.svelte
@@ -1,141 +1,141 @@
 <script>
-    window.scrollTo({ top: 0, behavior: "smooth" });
-    import { Link } from "svelte-routing";
-    import Nav from "./Nav.svelte";
-    import Image from "../assets/paneer.jpg";
-    import Stopwatch from "../assets/stopwatch.svg";
-    import Hourglass from "../assets/hourglass-end.svg";
-    import Smile from "../assets/grin-alt.svg";
-    import Muscle from "../assets/muscle.svg";
+  window.scrollTo({ top: 0, behavior: "smooth" });
+  import { Link } from "svelte-routing";
+  import Nav from "./Nav.svelte";
+  import Image from "../assets/paneer.jpg";
+  import Stopwatch from "../assets/stopwatch.svg";
+  import Hourglass from "../assets/hourglass-end.svg";
+  import Smile from "../assets/grin-alt.svg";
+  import Muscle from "../assets/muscle.svg";
 
-    // Sample JSON data (replace with your actual data fetching)
-    let recipeData = {
-        data: {
-            calories: 200,
-            expiry_date: "2024-04-20", // Example date format
-            id: 1,
-            name: "butter",
-            quantity: 5,
-        },
-        message: "Retrieved Recipe",
-    };
+  // Sample JSON data (replace with your actual data fetching)
+  let recipeData = {
+    data: {
+      calories: 200,
+      expiry_date: "2024-04-20", // Example date format
+      id: 1,
+      name: "butter",
+      quantity: 5,
+    },
+    message: "Retrieved Recipe",
+  };
 
-    // Function to format the expiry date (customize as needed)
-    function formatDate(dateString) {
-        const date = new Date(dateString);
-        const options = { year: "numeric", month: "long", day: "numeric" };
-        return date.toLocaleDateString(undefined, options); // Uses user's locale
-    }
+  // Function to format the expiry date (customize as needed)
+  function formatDate(dateString) {
+    const date = new Date(dateString);
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return date.toLocaleDateString(undefined, options); // Uses user's locale
+  }
 
-    import { Camera, CameraResultType, CameraSource } from "@capacitor/camera";
+  import { Camera, CameraResultType, CameraSource } from "@capacitor/camera";
 </script>
 
 <Nav />
 <main>
-    <div class="contents">
-        <h1>
-            <span>Here's your</span>
-            <br />
-            Recipe
-        </h1>
-        <h3>Pineapple Curry</h3>
-        <div class="recipe">
-            <img class="recipe__banner" src={Image} alt="banner" />
+  <div class="contents">
+    <h1>
+      <span>Here's your</span>
+      <br />
+      Recipe
+    </h1>
+    <h3>Pineapple Curry</h3>
+    <div class="recipe">
+      <img class="recipe__banner" src={Image} alt="banner" />
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet saepe
+        atque, reiciendis, veritatis facilis aliquam consequuntur facere ullam
+        illum cupiditate consequatur, expedita iure vero. Animi dicta numquam
+        soluta doloremque porro.
+      </p>
+      <div class="recipe__details">
+        <div class="recipe__details__row">
+          <div class="recipe__detail">
+            <h5>Ingredients Expiring in</h5>
+            {#if recipeData.data.expiry_date}
+              <p>
+                <img src={Hourglass} alt="icon" />
+                <b>{formatDate(recipeData.data.expiry_date)}</b>
+              </p>
+            {:else}
+              <p><img src={Hourglass} alt="icon" /> <b>N/A</b></p>
+            {/if}
+          </div>
+          <div class="recipe__detail">
+            <h5>Can be cooked in</h5>
             <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet
-                saepe atque, reiciendis, veritatis facilis aliquam consequuntur
-                facere ullam illum cupiditate consequatur, expedita iure vero.
-                Animi dicta numquam soluta doloremque porro.
+              <img src={Stopwatch} alt="icon" /> <b>20 Min</b>
             </p>
-            <div class="recipe__details">
-                <div class="recipe__details__row">
-                    <div class="recipe__detail">
-                        <h5>Ingredients Expiring in</h5>
-                        {#if recipeData.data.expiry_date}
-                            <p>
-                                <img src={Hourglass} alt="icon" />
-                                <b>{formatDate(recipeData.data.expiry_date)}</b>
-                            </p>
-                        {:else}
-                            <p><img src={Hourglass} alt="icon" /> <b>N/A</b></p>
-                        {/if}
-                    </div>
-                    <div class="recipe__detail">
-                        <h5>Can be cooked in</h5>
-                        <p>
-                            <img src={Stopwatch} alt="icon" /> <b>20 Min</b>
-                        </p>
-                    </div>
-                </div>
-                <div class="recipe__details__row">
-                    <div class="recipe__detail">
-                        <h5>Happyness Index</h5>
-                        <p><img src={Smile} alt="icon" /> <b>2D</b></p>
-                    </div>
-                    <div class="recipe__detail">
-                        <h5>Calorie Count</h5>
-                        {#if recipeData.data.calories}
-                            <p>
-                                <img src={Muscle} alt="icon" />
-                                <b>{recipeData.data.calories}</b>
-                            </p>
-                        {:else}
-                            <p><img src={Muscle} alt="icon" /> <b>N/A</b></p>
-                        {/if}
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
-        <h3>Recipe</h3>
-        <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis enim
-            sunt praesentium iure amet voluptatum cumque ut officia maiores
-            pariatur! Obcaecati quisquam odit enim nesciunt quia. Ad provident
-            repudiandae, rem ullam iure iste aut assumenda. <br /><br />Maxime
-            dolorem dolore beatae incidunt distinctio tenetur assumenda, qui
-            asperiores nam excepturi reiciendis voluptatum quae minima officiis
-            amet totam dicta sequi illo, voluptatibus soluta consectetur
-            blanditiis sapiente voluptate in! Cupiditate fugiat vel odit
-            officiis magni, minus voluptas, vitae dicta illum enim ex excepturi
-            quidem? Ex dolore aut tempore quos id! Odit architecto a ab commodi!
-        </p>
+        <div class="recipe__details__row">
+          <div class="recipe__detail">
+            <h5>Happyness Index</h5>
+            <p><img src={Smile} alt="icon" /> <b>2D</b></p>
+          </div>
+          <div class="recipe__detail">
+            <h5>Calorie Count</h5>
+            {#if recipeData.data.calories}
+              <p>
+                <img src={Muscle} alt="icon" />
+                <b>{recipeData.data.calories}</b>
+              </p>
+            {:else}
+              <p><img src={Muscle} alt="icon" /> <b>N/A</b></p>
+            {/if}
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="buttons">
-        <Link to="/dashboard">
-            <button>Cook</button>
-        </Link>
-    </div>
+    <h3>Recipe</h3>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis enim sunt
+      praesentium iure amet voluptatum cumque ut officia maiores pariatur!
+      Obcaecati quisquam odit enim nesciunt quia. Ad provident repudiandae, rem
+      ullam iure iste aut assumenda. <br /><br />Maxime dolorem dolore beatae
+      incidunt distinctio tenetur assumenda, qui asperiores nam excepturi
+      reiciendis voluptatum quae minima officiis amet totam dicta sequi illo,
+      voluptatibus soluta consectetur blanditiis sapiente voluptate in!
+      Cupiditate fugiat vel odit officiis magni, minus voluptas, vitae dicta
+      illum enim ex excepturi quidem? Ex dolore aut tempore quos id! Odit
+      architecto a ab commodi!
+    </p>
+  </div>
+  <div class="buttons">
+    <Link to="/dashboard">
+      <button>Cook</button>
+    </Link>
+  </div>
 </main>
 
 <style>
-    h3 {
-        margin-bottom: 1rem;
-    }
-    .recipe {
-        margin-bottom: 2rem;
-    }
-    .recipe__banner {
-        width: 100%;
-        border-radius: 1rem;
-        margin: 1rem 0;
-    }
-    .recipe__details__row {
-        display: flex;
-        justify-content: space-between;
-    }
-    .recipe__details img {
-        width: 1rem;
-        filter: invert(16%) sepia(19%) saturate(0%) hue-rotate(267deg)
-            brightness(102%) contrast(90%);
-    }
-    .recipe__detail {
-        padding-top: 2rem;
-        width: 50%;
-    }
-    .recipe__detail p {
-        padding-top: 0.5rem;
-        display: flex;
-        align-items: center;
-        gap: 0.5rem;
-    }
+  h3 {
+    margin-bottom: 1rem;
+  }
+  .recipe {
+    margin-bottom: 2rem;
+  }
+  .recipe__banner {
+    width: 100%;
+    border-radius: 1rem;
+    margin: 1rem 0;
+  }
+  .recipe__details__row {
+    display: flex;
+    justify-content: space-between;
+  }
+  .recipe__details img {
+    width: 1rem;
+    filter: invert(16%) sepia(19%) saturate(0%) hue-rotate(267deg)
+      brightness(102%) contrast(90%);
+  }
+  .recipe__detail {
+    padding-top: 2rem;
+    width: 50%;
+  }
+  .recipe__detail p {
+    padding-top: 0.5rem;
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  }
 </style>
