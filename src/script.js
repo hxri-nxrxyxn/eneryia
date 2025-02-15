bbdc904f80bf943455f322a3f14386139dc49570
Hari Narayan <harinarayanmr@aol.com>
2025-02-15 12:13:27 +0530

    going production ready

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 import { App } from "@capacitor/app";
 import { navigate } from "svelte-routing";
-import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
+import { BarcodeScanner } from "@capacitor-community/barcode-scanner";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { Storage } from "@capacitor/storage";
 
@@ -35,7 +35,6 @@ const startScanning = async () => {
   const result = await BarcodeScanner.startScan();
   document.body.style.background = "black";
 
-
   if (result.hasContent) {
     const code = result.content;
     const response = await fetch(
@@ -53,37 +52,39 @@ const startScanning = async () => {
       return;
     }
 
-    const name = res.product.product_name
-    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" ,"generationConfig": {
-      "response_mime_type": "application/json",
-}});
-    const prompt = `${name} for this product send only a json containing {name,calorie,quantity,expiry} if expiry is not sure make an estimate expiry is in days, the product quantity being then number of the products`;
-  const result2 = await model.generateContent(prompt);
-    const output = result2.response.text()
-    const out = JSON.parse(output)
-  console.log(out)
-  if(!out.products) {
-    const response = await fetch(`${baseUrl}/ingredient`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
+    const name = res.product.product_name;
+    const model = genAI.getGenerativeModel({
+      model: "gemini-2.0-flash",
+      generationConfig: {
+        response_mime_type: "application/json",
       },
-      body: JSON.stringify({
-        name : out.name,
-        calorie : Number(out.calorie),
-        quantity : Number(out.quantity),
-        expiry : out.expiry
-      }),
     });
-    const res = await response.json();
-    if(!response.ok) {
-      console.log(res)
-      return
+    const prompt = `${name} for this product send only a json containing {name,calorie,quantity,expiry} if expiry is not sure make an estimate expiry is in days, the product quantity being then number of the products`;
+    const result2 = await model.generateContent(prompt);
+    const output = result2.response.text();
+    const out = JSON.parse(output);
+    console.log(out);
+    if (!out.products) {
+      const response = await fetch(`${baseUrl}/ingredient`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: out.name,
+          calorie: Number(out.calorie),
+          quantity: Number(out.quantity),
+          expiry: out.expiry,
+        }),
+      });
+      const res = await response.json();
+      if (!response.ok) {
+        console.log(res);
+        return;
+      }
+      console.log(res);
+      return;
     }
-    console.log(res)
-    return
-  }
-
   } else {
     console.log("No content scanned");
   }
@@ -276,140 +277,149 @@ async function getIngredients(ingid) {
 }
 
 async function runAI(base64) {
-  try{
-  const prompt = "if the image has any products in it send only a json containing {name,calorie,quantity,expiry} if expiry is not sure make an estimate expiry is in days, the product quantity being then number of the products visible";
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" ,"generationConfig": {
-            "response_mime_type": "application/json",
-      }},
-    
-  );
-
-  console.log('t2')
-
-  const imageParts = [
-    { inlineData: {
-      data: base64,
-      mimeType: "image/jpeg",},
-    },
-  ];
-
-  console.log('t3')
-
-  const generatedContent = await model.generateContent([prompt, ...imageParts]);
-  
-  console.log('t4')
-
-  const output = generatedContent.response.text()
-  const out = JSON.parse(output)
-  console.log(out)
-  if(!out.products) {
-    const response = await fetch(`${baseUrl}/ingredient`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
+  try {
+    const prompt =
+      "if the image has any products in it send only a json containing {name,calorie,quantity,expiry} if expiry is not sure make an estimate expiry is in days, the product quantity being then number of the products visible";
+    const model = genAI.getGenerativeModel({
+      model: "gemini-1.5-pro",
+      generationConfig: {
+        response_mime_type: "application/json",
       },
-      body: JSON.stringify({
-        name : out.name,
-        calorie : Number(out.calorie),
-        quantity : Number(out.quantity),
-        expiry : out.expiry
-      }),
-    });
-    const res = await response.json();
-    if(!response.ok) {
-      console.log(res)
-      return
-    }
-    console.log(res)
-    const userdata = await checkUser();
-    const userid = userdata.id
-    const id = res.data.id;
-    const response3 = await fetch(`${baseUrl}/ingredient/${userid}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        ingid : id,
-      }),
     });
 
-    const res3 = await response3.json();
-    if(!response3.ok){
-      alert(res3.message)
-      return
+    console.log("t2");
+
+    const imageParts = [
+      {
+        inlineData: {
+          data: base64,
+          mimeType: "image/jpeg",
+        },
+      },
+    ];
+
+    console.log("t3");
+
+    const generatedContent = await model.generateContent([
+      prompt,
+      ...imageParts,
+    ]);
+
+    console.log("t4");
+
+    const output = generatedContent.response.text();
+    const out = JSON.parse(output);
+    console.log(out);
+    if (!out.products) {
+      const response = await fetch(`${baseUrl}/ingredient`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: out.name,
+          calorie: Number(out.calorie),
+          quantity: Number(out.quantity),
+          expiry: out.expiry,
+        }),
+      });
+      const res = await response.json();
+      if (!response.ok) {
+        console.log(res);
+        return;
+      }
+      console.log(res);
+      const userdata = await checkUser();
+      const userid = userdata.id;
+      const id = res.data.id;
+      const response3 = await fetch(`${baseUrl}/ingredient/${userid}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          ingid: id,
+        }),
+      });
+
+      const res3 = await response3.json();
+      if (!response3.ok) {
+        alert(res3.message);
+        return;
+      }
+      console.log(res3);
+
+      alert("Product added");
+      return;
     }
-    console.log(res3)
-    
-    alert("Product added")
-    return
+
+    const arr = out.products;
+
+    const data = arr.map(async (d) => {
+      const response = await fetch(`${baseUrl}/ingredient`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: d.name,
+          calorie: d.calorie,
+          quantity: d.quantity,
+          expiry: d.expiry,
+        }),
+      });
+      const res = await response.json();
+      if (!response.ok) {
+        alert(res.message);
+      }
+      console.log(res);
+    });
+  } catch (error) {
+    console.log(error);
   }
-
-  const arr = out.products;
-
-  const data = arr.map(async(d) => {
-    const response = await fetch(`${baseUrl}/ingredient`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name : d.name,
-        calorie : d.calorie,
-        quantity : d.quantity,
-        expiry : d.expiry
-      }),
-    });
-    const res = await response.json();
-    if(!response.ok) {
-      alert(res.message)
-    }
-    console.log(res)
-  })
-}
-catch(error) {
-  console.log(error);
-}
 }
 
 async function generteRecipe() {
   const userdata = await checkUser();
   const ingid = userdata.ingid;
   const ingarr = await getIngredients(ingid);
-  const text = JSON.stringify(ingarr)
+  const text = JSON.stringify(ingarr);
 
-  const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" ,"generationConfig": {
-    "response_mime_type": "application/json",
-}});
-  const prompt = `${text} these are the ingredients which are nearing expiry genrate me in json all the recipies that can be made with it in this format {name,instruction,cooking_time,happy,calories,ingid } where instruction is cooking instruction for recipe, happy is the satisfaction score for the recipe, ingid is the id of the ingredients used in the recipe`;
-  console.log(prompt)
-  const result2 = await model.generateContent(prompt);
-  const output = result2.response.text()
-  const out = JSON.parse(output)
-console.log(out)
-const arr = out.map(async(d) => {
-  const response = await fetch(`${baseUrl}/recipe`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
+  const model = genAI.getGenerativeModel({
+    model: "gemini-2.0-flash",
+    generationConfig: {
+      response_mime_type: "application/json",
     },
-    body: JSON.stringify({
-      name : d.name,
-      instruction : d.instruction,
-      cooking_time : Number(d.cooking_time),
-      happy : d.happy,
-      calories : d.calories,
-      ingid : d.ingid
-    }),
   });
-  const res = await response.json();
-  if(!response.ok) {
-    alert(res.message)
-  }
-  console.log(res)
+  const prompt = `${text} these are the ingredients which are nearing expiry genrate me in json all the recipies that can be made with it in this format {name,instruction,cooking_time,happy,calories,ingid } where instruction is cooking instruction for recipe, happy is the satisfaction score for the recipe, ingid is the id of the ingredients used in the recipe`;
+  console.log(prompt);
+  const result2 = await model.generateContent(prompt);
+  const output = result2.response.text();
+  const out = JSON.parse(output);
+  console.log(out);
+  const arr = out.map(async (d) => {
+    const response = await fetch(`${baseUrl}/recipe`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: d.name,
+        instruction: d.instruction,
+        cooking_time: Number(d.cooking_time),
+        happy: d.happy,
+        calories: d.calories,
+        ingid: d.ingid,
+      }),
+    });
+    const res = await response.json();
+    if (!response.ok) {
+      alert(res.message);
+    }
+    console.log(res);
 
-  const userdata = await checkUser();
-    const userid = userdata.id
+    const userdata = await checkUser();
+    const userid = userdata.id;
     const id = res.data.id;
     const response3 = await fetch(`${baseUrl}/recipe/${userid}`, {
       method: "PUT",
@@ -417,24 +427,24 @@ const arr = out.map(async(d) => {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        recid : id,
+        recid: id,
       }),
     });
 
     const res3 = await response3.json();
-    if(!response3.ok){
-      alert(res3.message)
-      return
+    if (!response3.ok) {
+      alert(res3.message);
+      return;
     }
-    console.log(res3)
-    
-    alert("Product added")
-    return
-} )
+    console.log(res3);
+
+    alert("Product added");
+    return;
+  });
 }
 
-
 async function getRecipe(id) {
+  console.log(id);
   const response = await fetch(`${baseUrl}/recipe/${id}`, {
     method: "GET",
     headers: {
@@ -450,4 +460,19 @@ async function getRecipe(id) {
   return res.data;
 }
 
-export { handleBackButton, signup, checkPermission, startScanning, stopScanning, checkUser, logout, login, collect, getRecipies, getIngredients, runAI, generteRecipe, getRecipe };
+export {
+  handleBackButton,
+  signup,
+  checkPermission,
+  startScanning,
+  stopScanning,
+  checkUser,
+  logout,
+  login,
+  collect,
+  getRecipies,
+  getIngredients,
+  runAI,
+  generteRecipe,
+  getRecipe,
+};
